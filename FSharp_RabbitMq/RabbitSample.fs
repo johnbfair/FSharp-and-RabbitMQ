08825ece47f53b492a0161c350a287467a7557b0
John Fair <johnfair@quickenloans.com>
2013-12-30 17:02:13 -0500

    Initial commit

---
+++ FSharp_RabbitMq/RabbitSample.fs
@@ -0,0 +1,90 @@
+﻿module RabbitSample
+
+    open System
+    open System.Xml
+    open System.Xml.Linq
+    open FSharp_RabbitMq.Types
+       
+    type TheGrid(ack, message1CmdQ: RabbitMqPublisher, message2CmdQ: RabbitMqPublisher) =
+        let log msg value = () //printfn "Message: %s Value: %i" msg value
+
+        let supervisor = new Agent<System.Exception>(fun inbox ->
+                            let rec Loop() =
+                                async {
+                                    let! err = inbox.Receive()
+                                    log err.Message 0
+                                    do! Loop() }
+                            Loop()) |> Agent.start
+
+        let accountant = new Agent<int64>(fun inbox ->
+                            let rec Loop() =
+                                async {
+                                    let! count = inbox.Receive()
+                                    //log err.Message 0
+                                    do! Loop() }
+                            Loop()) |> Agent.start
+        
+        member this.RouteCommand(extMsg:string, tag:uint64) =
+            (new Agent<Command>(fun inbox ->
+                async {
+                        let watch = System.Diagnostics.Stopwatch.StartNew()
+
+                        let! msg = inbox.Receive()
+                        let msgType =
+                            match System.Text.RegularExpressions.Regex.Match(msg.Message, "<(?<tag>\w*)>") with
+                            | x when x.Success -> x.Captures.[0].Value
+                            | _ -> raise (new Exception "Couldn't determine message type (null root)")
+
+                        match msgType with
+                        | "Message1" ->                           
+                            (new Agent<Command>(fun inbox ->
+                                                    async {
+                                                        let! x = inbox.Receive()
+                                                        let! m = message1CmdQ.Send x.Message
+                                                        ack x.Tag
+                                                    }) |> Agent.reportErrorsTo supervisor |> Agent.start) 
+                            |> fun x-> x.Post msg
+
+                        | "Message2" -> 
+                            (new Agent<Command>(fun inbox ->
+                                                    async {
+                                                        let! x = inbox.Receive()
+                                                        let! m = message2CmdQ.Send x.Message
+                                                        ack x.Tag
+                                                    }) |> Agent.reportErrorsTo supervisor |> Agent.start) 
+                            |> fun x-> x.Post msg
+                        | _ -> ack msg.Tag; raise (new Exception "Invalid Command") 
+                        
+                        accountant.Post watch.ElapsedMilliseconds
+                        })
+                |> Agent.reportErrorsTo supervisor 
+                |> Agent.start)
+                |> fun x-> x.Post {Message=extMsg; Tag=tag}
+
+        member this.ProcessMessage1Command ack (extMsg:string, tag:uint64) =
+            (new Agent<Message1>(fun inbox ->
+                async {
+                    let! msg = inbox.Receive()
+                    log msg.Sample1 msg.Value1
+                    ack msg.MessageTag
+                })
+            |> Agent.reportErrorsTo supervisor 
+            |> Agent.start)
+            |> fun x-> 
+                let msg = Rock.Framework.Serialization.XmlObjectSerializer.XmlToObject<Message1>(extMsg) 
+                msg.MessageTag <- tag
+                msg |> x.Post
+
+        member this.ProcessMessage2Command ack (extMsg:string, tag:uint64) =
+            (new Agent<Message2>(fun inbox ->
+                async {
+                    let! msg = inbox.Receive()
+                    log msg.Sample2 msg.Value2
+                    ack msg.MessageTag
+                })
+            |> Agent.reportErrorsTo supervisor 
+            |> Agent.start)
+            |> fun x-> 
+                let msg = Rock.Framework.Serialization.XmlObjectSerializer.XmlToObject<Message2>(extMsg) 
+                msg.MessageTag <- tag
+                msg |> x.Post
---
+++ FSharp_RabbitMq/Types.fs
@@ -0,0 +1,75 @@
+﻿namespace FSharp_RabbitMq.Types
+
+open RabbitMQ.Client
+
+type Agent<'T> = MailboxProcessor<'T>
+
+type Credentials = { Host:string; Username:string; Password:string }
+
+type Command = {Message:string; Tag:uint64}
+
+type PublishType =
+    | Exchange of string
+    | Queue of string
+
+type CounterMsg =
+    | Add of int64
+    | GetAndReset of (int64 -> unit)
+
+type Message1(sample1, value1, messageTag) = 
+    member val Sample1 = sample1 with get,set
+    member val Value1 = value1 with get,set
+    member val MessageTag = messageTag with get,set
+    new () = Message1("", 0, 0UL)
+
+type Message2(sample2, value2, messageTag) = 
+    member val Sample2 = sample2 with get,set
+    member val Value2 = value2 with get,set
+    member val MessageTag = messageTag with get,set
+    new () = Message2("", 0, 0UL)
+        
+type RabbitMqPublisher (creds, publishType) = 
+    let connection = 
+        let connectionFactory = new ConnectionFactory()
+        connectionFactory.UserName <- creds.Username
+        connectionFactory.Password <- creds.Password
+        connectionFactory.Uri <- creds.Host
+        connectionFactory.CreateConnection()
+    let model = 
+        let m = connection.CreateModel()
+        match publishType with
+        | Queue x -> m.QueueDeclare(x, true, false, false, null) |> ignore
+        | _ -> ()
+        m
+    let properties = 
+        let p = model.CreateBasicProperties()
+        p.DeliveryMode <- 2uy; p
+    member this.Send (msg:string) = 
+        match publishType with
+        | Queue x -> async { model.BasicPublish("", x, properties, System.Text.Encoding.ASCII.GetBytes msg) }
+        | Exchange x -> async { model.BasicPublish(x, "", properties, System.Text.Encoding.ASCII.GetBytes msg) }
+
+type RabbitMqSubscriber(creds:Credentials, queue: string) = 
+    let connection = 
+        let connectionFactory = new ConnectionFactory()
+        connectionFactory.Uri <- creds.Host
+        connectionFactory.UserName <- creds.Username
+        connectionFactory.Password <- creds.Password
+        connectionFactory.CreateConnection()
+    let model = 
+        let m = connection.CreateModel()
+        m.QueueDeclare(queue, true, false, false, null) |> ignore
+        //m.QueueBind(queue, "amq.direct", "")
+        m
+    let receiveMessage f = new Events.BasicDeliverEventHandler(fun sender args -> f (System.Text.Encoding.ASCII.GetString args.Body) args.DeliveryTag)
+    let consumer = new Events.EventingBasicConsumer (Model= model)
+    member this.BindReceivedEvent f = consumer.add_Received(receiveMessage f)
+    member this.Start = model.BasicConsume(queue, false, consumer)
+    member this.Working = consumer.IsRunning
+    member this.AckMessage tag = model.BasicAck(tag, false)
+
+module Agent =
+    let reportErrorsTo (supervisor: Agent<exn>) (agent: Agent<_>) =
+        agent.Error.Add(fun error -> supervisor.Post error); agent
+
+    let start (agent: Agent<_>) = agent.Start(); agent   
